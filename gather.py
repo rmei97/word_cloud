85a191dd52f65002303395413fb6d02214986443
Richard Mei <richard.mei97@gmail.com>
2020-10-25 20:15:24 -0400

    ready to scrape

--- gather.py
+++ gather.py
@@ -3,6 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+import sqlite3
+import os
+
 def job_info_from_soup(soup):
 	# temp = soup.find('div',class_ = "topcard__content-left")
 	#variables
@@ -13,18 +16,49 @@ def job_info_from_soup(soup):
 	# salary = soup.find('div',class_ = "salary topcard__flavor--salary").text
 	return [title, company, location, description]
 
+def connect_to_db():
+	"""Create a connection to a jobs.db. Useful if I want to update every x scrapes """
+	conn = sqlite3.connect('jobs.db')
+	c = conn.cursor()
+	try:
+	    query = """CREATE TABLE listings (index INTEGER PRIMARY KEY, title TEXT NOT NULL, company TEXT NOT NULL, location TEXT NOT NULL, description TEXT NOT NULL)"""
+	    c.execute(query)
+	except: #trying to catch OperationalError but will commit a sin of except pass:
+		pass
 
+def insert():
+	return None
+
+###### turn the contents into soup and store all the links into jobs variable
 with open('jobs.html', 'r') as file:
     contents = file.read()
-#turn the contents into soup and store all the links into jobs variable
     search_soup = BeautifulSoup(contents, 'lxml')
     jobs_list = search_soup.findAll('a', class_ = "result-card__full-card-link")
+#####
 
+#### script
+conn = sqlite3.connect('jobs.db')
+c = conn.cursor()
 
+query = """CREATE TABLE IF NOT EXISTS listings (num INTEGER PRIMARY KEY, title TEXT NOT NULL, company TEXT NOT NULL, location TEXT NOT NULL, description TEXT NOT NULL)"""
+c.execute(query)
+
+   
+index = 1
 for job_link in jobs_list[:3]:
 	request = requests.get(job_link['href'])
 	time.sleep(1)
 	print('sleep')
 	posting_soup = BeautifulSoup(request.content, 'lxml')
+	
 	data = job_info_from_soup(posting_soup)
+	data = [index] + data
+	query = """INSERT INTO listings VALUES(?, ?, ?, ?, ?);"""
+	c.execute(query, data)
+
+	index += 1
+	print('Stored')
+conn.commit()
+conn.close()
+print('Connection Closed')
 
